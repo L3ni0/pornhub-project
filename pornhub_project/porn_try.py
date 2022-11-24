c23ea47eea80e4574829b5b2f2966eb02ebc59a8
Your Name <you@example.com>
2022-11-24 12:48:21 +0100

    v1.1

--- pornhub_project/porn_try.py
+++ pornhub_project/porn_try.py
@@ -14,18 +14,22 @@ api = PornhubApi()
 
 # conn.execute('CREATE TABLE IF NOT EXISTS data (vid_id text PRIMARY KEY, pornstars text, title text)')
 # print(pd.read_sql("SELECT * FROM information_schema.columns WHERE table_name = 'data'", engine))
-
-
-data = api.search.search(
-    ordering="mostviewed",
-    period="all-time",
-)
-
-
 G = PornGraph()
-
-for vid in data.videos:
-    if vid.pornstars:
-        G.add_vid(vid)
+for i in range(1,20):
+    data = api.search.search(
+        ordering="mostviewed",
+        period="all-time",
+        page=i
+    )
+    print(f'download {i}')
+    for vid in data.videos:
+        if vid.pornstars:
+            G.add_vid(vid)
         
+    print(i)
+
+
+
+
+
 G.show('nx.html')
